09cf11fab872cc60b54c7018e9de14e391593815
Pozitrone <jirirkas.skola@gmail.com>
2021-01-15 14:29:24 +0100

    now getting python

--- Source/WakaTimeForUE4/Private/WakaTimeForUE4.cpp
+++ Source/WakaTimeForUE4/Private/WakaTimeForUE4.cpp
@@ -1,16 +1,54 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "WakaTimeForUE4.h"
+#include "GenericPlatform/GenericPlatformMisc.h"
+#include "Misc/MessageDialog.h"
+#include <string>
+#include <array>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+
+
+using namespace std;
+using namespace EAppMsgType;
 
 #define LOCTEXT_NAMESPACE "FWakaTimeForUE4Module"
+
+void CheckForPython() {
+	UE_LOG(LogTemp, Warning, TEXT("Checking Python installation..."));
+	char buffer[128];
+	string result = "";
+	string py = "Python 3";
+	FILE* pipe;
+
+	pipe = _popen("python --version", "r");
+
+	while (!feof(pipe)) {
+
+		if (fgets(buffer, 128, pipe) != NULL)
+			result += buffer;
+	}
+
+	if (result.find(py) != std::string::npos) {
+		UE_LOG(LogTemp, Warning, TEXT("Python found."));
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("Python not found. Please, install it and restart the editor."));
+		_pclose(_popen("python", "r")); // Opens Microsoft Store
+		FMessageDialog::Open(Ok, FText::FromString("Python not found. Please, install it and restart the editor."));
+		FGenericPlatformMisc::RequestExit(false); // Quits the editor
+		
+	}
+
+	_pclose(pipe);
+}
 
 void FWakaTimeForUE4Module::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	char buffer[128];
-	FILE* pPipe;
-
-	char* result = _popen("python --version", "r").;
+	CheckForPython();
+	// char* result = _popen("python --version", "r");
 }
 
 void FWakaTimeForUE4Module::ShutdownModule()
