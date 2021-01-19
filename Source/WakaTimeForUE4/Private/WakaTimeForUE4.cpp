b8c9badd4c61e3ecde2a62fffc9762974c0ae587
Pozitrone <jirirkas.skola@gmail.com>
2021-01-19 21:41:56 +0100

    Added event unbinding

--- Source/WakaTimeForUE4/Private/WakaTimeForUE4.cpp
+++ Source/WakaTimeForUE4/Private/WakaTimeForUE4.cpp
@@ -223,6 +223,10 @@ void FWakaTimeForUE4Module::ShutdownModule()
 {
 
 	FEditorDelegates::OnNewActorsDropped.Remove(NewActorsDroppedHandle);
+	FEditorDelegates::OnDeleteActorsEnd.Remove(DeleteActorsEndHandle);
+	FEditorDelegates::OnDuplicateActorsEnd.Remove(DuplicateActorsEndHandle);
+	FEditorDelegates::OnNewActorsDropped.Remove(AddLevelToWorldHandle);
+	FEditorDelegates::OnNewActorsDropped.Remove(PostSaveWorldHandle);
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
