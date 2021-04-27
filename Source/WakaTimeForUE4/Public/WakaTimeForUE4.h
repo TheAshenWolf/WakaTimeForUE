dbd8f20a597a09c5a4d09cfd2529396dc173a60e
Pozitrone <jirirkas.skola@gmail.com>
2021-04-27 16:52:07 +0100

    version bump

--- Source/WakaTimeForUE4/Public/WakaTimeForUE4.h
+++ Source/WakaTimeForUE4/Public/WakaTimeForUE4.h
@@ -8,8 +8,6 @@
 class FWakaTimeForUE4Module : public IModuleInterface
 {
 public:
-
-
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
